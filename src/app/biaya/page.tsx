890638981efbeaff9deb03e808ff1c097df912c6
VMMS-SIG <vmms-sig@local>
2026-09-13 09:15:33 +0000

    fix: close sparepart table with Fragment not empty tag

--- src/app/biaya/page.tsx
+++ src/app/biaya/page.tsx
@@ -325,7 +325,7 @@ export default function Biaya() {
                         </td>
                       </tr>
                     )}
-                  </>
+                  </Fragment>
                 );
               })}
             </tbody>
